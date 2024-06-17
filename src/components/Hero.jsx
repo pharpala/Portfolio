1b97fce2d38e19a3e602eb3867df623ff2677d7d
Pranay Harpalani <pranayinspain@gmail.com>
2024-06-16 23:07:27 -0400

    test for my iphone screen

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -33,7 +33,7 @@ const Hero = () => {
     mx-auto pb-10"> 
 
       <div className={`${styles.paddingX} absolute
-      inset-0 ${isMobile ? 'top-[140px]' : 'top-[90px]'} max-w-7xl mx-auto flex
+      inset-0 ${isMobile ? 'top-[100px]' : 'top-[90px]'} max-w-7xl mx-auto flex
       flex-row items-start gap-5 ]`}>
         <div className="flex flex-col
         justify-center items-center mt-5">
@@ -44,7 +44,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white 
-            ${isMobile ? 'text-[80px]' : 'text-[40px]'}`}> Hi, I'm <span className="text-[#915eff]">Pranay</span></h1>
+            ${isMobile ? 'text-[75px]' : 'text-[40px]'}`}> Hi, I'm <span className="text-[#915eff]">Pranay</span></h1>
           <p className={`${styles.heroSubText}
           text-white-100 ${isMobile ? 'text-[25px]' : 'text-[16px]'}`} >
             I make front-end interfaces <br className="block xs:hidden"/> 
