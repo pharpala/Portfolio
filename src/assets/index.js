2b2f57447330fa1233995019080652770091bd3d
Pranay Harpalani <pranayinspain@gmail.com>
2024-06-18 01:56:28 -0400

    added work experience

--- src/assets/index.js
+++ src/assets/index.js
@@ -29,6 +29,10 @@ import tesla from "./company/tesla.png";
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
+import hp from "./hp.png";
+import teachify from "./teachify.png";
+import gdsc from "./gdsc.png";
+import mint from "./mint.png";
 
 export {
   logo,
@@ -59,4 +63,8 @@ export {
   carrent,
   jobit,
   tripguide,
+  hp,
+  teachify,
+  gdsc,
+  mint,
 };
