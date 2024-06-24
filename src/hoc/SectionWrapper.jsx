d78c9dc920f69b3ed1198ff531ca09c5b7b0037e
Pranay Harpalani <pranayinspain@gmail.com>
2024-06-24 18:01:35 -0400

    fixed responsiveness

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -10,7 +10,7 @@ const StarWrapper = (Component, idName) =>
         variants={staggerContainer()}
         initial='hidden'
         whileInView='show'
-        viewport={{ once: true, amount: 0.25 }}
+        viewport={{ once: true, amount: 0.15 }}
         className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
       >
         <span className='hash-span' id={idName}>
